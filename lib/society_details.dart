105fd41afcb3345ed832c66fa16f08e5fcbb3c37
chetanyk655 <printopia06@gmail.com>
2024-09-14 15:09:58 +0530

    Society details widget added

---
+++ lib/society_details.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SocietyDetails extends StatefulWidget {
+  const SocietyDetails({super.key});
+
+  @override
+  State<SocietyDetails> createState() {
+    return _SocietyDetailsState();
+  }
+}
+
+class _SocietyDetailsState extends State<SocietyDetails> {
+  @override
+  Widget build(context) {
+    return const Text("Hello");
+  }
+}
