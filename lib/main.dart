4ab135877dc6d6ca12d9e603b4461d03bf7b2596
chetanyk655 <printopia06@gmail.com>
2024-09-16 15:17:24 +0530

    Added emergency_contacts

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:first_app/society_details.dart';
-import 'package:flutter/rendering.dart';
+// import 'package:first_app/society_details.dart';
+// import 'package:first_app/emergency_contacts.dart';
+// import 'package:flutter/rendering.dart';
 
 void main() {
   // debugPaintSizeEnabled = true;
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
-  ]).then((fn) {
-    runApp(const SocietyDetails());
-  });
+  ]).then((fn) {});
 }
